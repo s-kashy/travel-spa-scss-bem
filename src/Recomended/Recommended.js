8b3cdbf36749aa692339cc2b50e011310b04bc07
s-kashy <shlomo.kashy@gmail.com>
2019-07-09 21:57:48 +0300

    added left panel component

--- src/Recomended/Recommended.js
+++ src/Recomended/Recommended.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
-import {Recommend,photo,friend} from "./Recommended.module.scss"
-import user4 from "../img/user-4.jpg"
-import  {paragraph} from "../scss/typogrphe.module.scss"
+import React, { Component } from "react";
+import { Recommend, photo, friend } from "./Recommended.module.scss";
+import user4 from "../img/user-4.jpg";
+import { paragraph } from "../scss/typogrphe.module.scss";
 
 export default class Recommended extends Component {
-    render() {
-        return (
-            <div className={Recommend}>
-            <p className={paragraph}>The friend that recommend this </p>
-                <div className={friend}>
-                <img className={photo} src={user4} alt="friend"/>
-                <img className={photo}  src={user4} alt="friend"/>
-                <img className={photo}  src={user4} alt="friend"/>
-                </div>
-              
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={Recommend}>
+        <p className={paragraph}>The friend that recommend this </p>
+        <div className={friend}>
+          <img className={photo} src={user4} alt="friend" />
+          <img className={photo} src={user4} alt="friend" />
+          <img className={photo} src={user4} alt="friend" />
+        </div>
+      </div>
+    );
+  }
 }
